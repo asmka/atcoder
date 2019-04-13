aa620d092631f2ea34baabd1e17a914a257def25
knock <dff9th@gmail.com>
2019-04-14 00:10:47 +0900

    [fix] Refactoring Problem D

--- abc124/d.py
+++ abc124/d.py
@@ -1,57 +1,34 @@
 (N, K) = map(int, input().split())
 S = input()
 
-ans = 0
-fpos = []
-p = '0'
+fpos_list = []
+ps = ''
 for i in range(len(S)):
-    if p == '0' and S[i] == '1':
-        fpos.append(i)
-    if p == '1' and S[i] == '0':
-        fi = len(fpos) - 1 - K
-        if fi >= 0:
-            l = (i-1) - (fpos[fi]-1)
-            if l > ans:
-                ans = l
-    p = S[i]
-#print('ans: ', ans)
-            
-# top
-p = '1'
-cnt = 0
-for i in range(len(S)):
-    if p == '0' and S[i] == '1':
-        cnt += 1
-    if p == '1' and S[i] == '0':
-        if cnt == K:
-            l = i
-            if l > ans:
-                ans = l
-            break
-    if i == len(S) - 1:
-        l = len(S)
-        if l > ans:
-            ans = l
-        break
-    p = S[i]
+    if ps != S[i]:
+        fpos_list.append([i, S[i]])
+        ps = S[i]
 
-# tail
-p = '1'
-cnt = 0
-for i in range(len(S)-1, -1, -1):
-    if p == '0' and S[i] == '1':
-        cnt += 1
-    if p == '1' and S[i] == '0':
-        if cnt == K:
-            l = (len(S)-1) - i
-            if l > ans:
-                ans = l
-            break
-    if i == 0:
-        l = len(S)
-        if l > ans:
-            ans = l
-        break
-    p = S[i]
+ans = 0
+for i in range(len(fpos_list)):
+    bp = fpos_list[i][0]
+    bs = fpos_list[i][1]
+    l = 0
+    if bs == '0':
+        ep = N
+        if i+2*K < len(fpos_list):
+            ep = fpos_list[i+2*K][0]
+        l = ep-bp
+    else:
+        ep = N
+        if i+1+2*K < len(fpos_list):
+            ep = fpos_list[i+1+2*K][0]
+        l = ep-bp
+    if l > ans:
+        ans = l
+    # debug
+    #print('i: ', i)
+    #print('bp: ', bp)
+    #print('bs: ', bs)
+    #print('l: ', l)
 
 print(ans)
