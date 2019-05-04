05c9ef1a6d5d51669989eafa6b521b2d71897e9e
knock <dff9th@gmail.com>
2019-05-05 01:49:31 +0900

    fix algorithm at A problem

--- agc033/a.py
+++ agc033/a.py
@@ -3,82 +3,31 @@ A = []
 for i in range(H):
     A.append(input())
 
-
-def fillDistance(d, h, w):
-    # up
-    ch = h - 1
-    cd = 1
-    while ch >= 0 and cd < d[ch][w]: 
-        d[ch][w] = cd
-        ch -= 1
-        cd += 1
-    # down
-    ch = h + 1
-    cd = 1
-    while ch < H and cd < d[ch][w]: 
-        d[ch][w] = cd
-        ch += 1
-        cd += 1
-    # right and updown
-    cw = w+1
-    cd = 1
-    while cw < W and cd < d[h][cw]:
-        d[h][cw] = cd
-        # up
-        ch = h - 1
-        cdd = cd + 1
-        while ch >= 0 and cdd < d[ch][cw]: 
-            d[ch][cw] = cdd
-            ch -= 1
-            cdd += 1
-        # down
-        ch = h + 1
-        cdd = cd + 1
-        while ch < H and cdd < d[ch][cw]: 
-            d[ch][cw] = cdd
-            ch += 1
-            cdd += 1
-        cd += 1
-        cw += 1
-    # left and updown
-    cw = w-1
-    cd = 1
-    while cw >= 0 and cd < d[h][cw]:
-        d[h][cw] = cd
-        # up
-        ch = h - 1
-        cdd = cd + 1
-        while ch >= 0 and cdd < d[ch][cw]: 
-            d[ch][cw] = cdd
-            ch -= 1
-            cdd += 1
-        # down
-        ch = h + 1
-        cdd = cd + 1
-        while ch < H and cdd < d[ch][cw]: 
-            d[ch][cw] = cdd
-            ch += 1
-            cdd += 1
-        cd += 1
-        cw -= 1
-    #print(d)
-
-
-d = [[10000 for j in range(W)] for i in range(H)]
+F = [[False for w in range(W)] for h in range(H)]
+blackList = []
 for h in range(H):
     for w in range(W):
         if A[h][w] == '#':
-            d[h][w] = 0
-for h in range(H):
-    for w in range(W):
-        if A[h][w] == '#':
-            #print('call fill', h, w)
-            fillDistance(d, h, w)
-                
-#print(d)
-ans = -1
-for h in range(H):
-    for w in range(W):
-        if d[h][w] > ans:
-            ans = d[h][w]
+            F[h][w] = True
+            blackList.append((h, w))
+
+ans = 0
+while blackList:
+    ans += 1
+    nextBlack = []
+    for (h, w) in nextBlack:
+        if h-1 >= 0 and F[h-1][w] == False:
+            F[h-1][w] = True
+            nextBlack.append((h-1, w))
+        if h+1 < H and F[h+1][w] == False:
+            F[h+1][w] = True
+            nextBlack.append((h+1, w))
+        if w-1 >= 0 and F[h][w-1] == False:
+            F[h][w-1] = True
+            nextBlack.append((h, w-1))
+        if w+1 >= 0 and F[h][w+1] == False:
+            F[h][w+1] = True
+            nextBlack.append((h, w+1))
+    blackList = nextBlack
+
 print(ans)
