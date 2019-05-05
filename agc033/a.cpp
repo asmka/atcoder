ee806d9254c9af2afb0b2d67e30997ec43847b2d
knock <dff9th@gmail.com>
2019-05-05 15:04:10 +0900

    use struct

--- agc033/a.cpp
+++ agc033/a.cpp
@@ -1,9 +1,14 @@
 #include <queue>
-#include <vector>
 #include <string>
 #include <iostream>
 
 using namespace std;
+
+struct PosRoute {
+    int h;
+    int w;
+    int d;
+};
 
 
 int main() {
@@ -15,14 +20,14 @@ int main() {
     }
 
     // initialize first black positions
-    queue<vector<int>*> black_list {};
+    queue<PosRoute> black_list {};
     int **f = new int*[H];
     for (int h=0; h<H; h++) {
         f[h] = new int[W];
         for (int w=0; w<W; w++) {
             if (A[h][w] == '#') {
                 f[h][w] = true;
-                black_list.push(new vector<int> {h, w, 0});
+                black_list.push({h, w, 0});
             } else {
                 f[h][w] = false;
             }
@@ -32,33 +37,32 @@ int main() {
     int ans = 0;
     // expand black positions until all positions fill
     while (!black_list.empty()) {
-        vector<int> *pos = black_list.front();
-        int h = (*pos)[0];
-        int w = (*pos)[1];
-        int d = (*pos)[2];
+        PosRoute pos = black_list.front();
+        int h = pos.h;
+        int w = pos.w;
+        int d = pos.d;
         // up
         if (h-1 >= 0 && f[h-1][w] == false) {
             f[h-1][w] = true;
-            black_list.push(new vector<int> {h-1, w, d+1});
+            black_list.push({h-1, w, d+1});
         }
         // down
         if (h+1 < H && f[h+1][w] == false) {
             f[h+1][w] = true;
-            black_list.push(new vector<int> {h+1, w, d+1});
+            black_list.push({h+1, w, d+1});
         }
         // left
         if (w-1 >= 0 && f[h][w-1] == false) {
             f[h][w-1] = true;
-            black_list.push(new vector<int> {h, w-1, d+1});
+            black_list.push({h, w-1, d+1});
         }
         // right
         if (w+1 < W && f[h][w+1] == false) {
             f[h][w+1] = true;
-            black_list.push(new vector<int> {h, w+1, d+1});
+            black_list.push({h, w+1, d+1});
         }
         ans = d;
         black_list.pop();
-        delete pos;
     }
 
     cout << ans << endl;
