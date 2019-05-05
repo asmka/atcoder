ff8d92cac2d2a75d5044034b32ada64c052a0292
knock <dff9th@gmail.com>
2019-05-05 13:59:50 +0900

    refactoring problem A

--- agc033/a.cpp
+++ agc033/a.cpp
@@ -1,114 +1,65 @@
-#include <map>
+#include <queue>
 #include <vector>
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-int H, W;
-
-
-void fillUp(int **d, int h, int w, int cur_distance) {
-    int ch = h - 1;
-    int cd = cur_distance + 1;
-    while (ch >= 0 && cd < d[ch][w]) {
-        d[ch][w] = cd;
-        ch -= 1;
-        cd += 1;
-    }
-}
-
-
-void fillDown(int **d, int h, int w, int cur_distance) {
-    int ch = h + 1;
-    int cd = cur_distance + 1;
-    while (ch < H && cd < d[ch][w]) {
-        d[ch][w] = cd;
-        ch += 1;
-        cd += 1;
-    }
-}
-
-void debug(int **d) {
-    //cout << "H: " << H << endl;
-    //cout << "W: " << W << endl;
-    for (int h=0; h<H; h++) {
-        for (int w=0; w<W; w++) {
-            cout << d[h][w] << ' ';
-        }
-        cout << endl;
-    }
-}
-
-void fillDistance(int** d, int h, int w) {
-    int cw, cd;
-
-    // updown
-    fillUp(d, h, w, 0);
-    fillDown(d, h, w, 0);
-    //debug(d);
-
-    // left
-    cw = w - 1;
-    cd = 1;
-    while (cw >= 0 && cd < d[h][cw]) {
-        d[h][cw] = cd;
-        fillUp(d, h, cw, cd);
-        fillDown(d, h, cw, cd);
-        cw -= 1;
-        cd += 1;
-    }
-
-    // right
-    cw = w + 1;
-    cd = 1;
-    while (cw < W && cd < d[h][cw]) {
-        d[h][cw] = cd;
-        fillUp(d, h, cw, cd);
-        fillDown(d, h, cw, cd);
-        cw += 1;
-        cd += 1;
-    }
-}
-
 
 int main() {
+    int H {}, W {};
     cin >> H >> W;
     string A[H];
     for (int h=0; h<H; h++) {
         cin >> A[h];
     }
-    int **d = new int*[H];
+
+    // initialize first black positions
+    queue<vector<int>*> black_list {};
+    int **f = new int*[H];
     for (int h=0; h<H; h++) {
-        d[h] = new int[W];
+        f[h] = new int[W];
         for (int w=0; w<W; w++) {
             if (A[h][w] == '#') {
-                d[h][w] = 0;
+                f[h][w] = true;
+                black_list.push(new vector<int> {h, w, 0});
             } else {
-                d[h][w] = 10000;
-            }
-            //cout << d[h][w];
-        }
-    }
-
-    for (int h=0; h<H; h++) {
-        for (int w=0; w<W; w++) {
-            if (A[h][w] == '#') {
-                fillDistance(d, h, w);
+                f[h][w] = false;
             }
         }
     }
 
-    int ans = -1;
-    for (int h=0; h<H; h++) {
-        for (int w=0; w<W; w++) {
-            //cout << d[h][w];
-            if (d[h][w] > ans) {
-                ans = d[h][w];
-            }
+    int ans = 0;
+    // expand black positions until all positions fill
+    while (!black_list.empty()) {
+        vector<int> *pos = black_list.front();
+        int h = (*pos)[0];
+        int w = (*pos)[1];
+        int d = (*pos)[2];
+        // up
+        if (h-1 >= 0 && f[h-1][w] == false) {
+            f[h-1][w] = true;
+            black_list.push(new vector<int> {h-1, w, d+1});
         }
+        // down
+        if (h+1 < H && f[h+1][w] == false) {
+            f[h+1][w] = true;
+            black_list.push(new vector<int> {h+1, w, d+1});
+        }
+        // left
+        if (w-1 >= 0 && f[h][w-1] == false) {
+            f[h][w-1] = true;
+            black_list.push(new vector<int> {h, w-1, d+1});
+        }
+        // right
+        if (w+1 < W && f[h][w+1] == false) {
+            f[h][w+1] = true;
+            black_list.push(new vector<int> {h, w+1, d+1});
+        }
+        ans = d;
+        black_list.pop();
+        delete pos;
     }
-    //debug(d);
 
     cout << ans << endl;
 }
