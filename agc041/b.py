d1c0a02ecdf8a4f92f39eb4a3f508debad1cb318
dff9th <dff9th@gmail.com>
2020-03-24 11:22:21 +0000

    tmp agc041 b

--- agc041/b.py
+++ agc041/b.py
@@ -1,19 +1,32 @@
-N, M, V, P = map(int, input().split())
-A = list(map(int, input().split()))
-
-A.sort(reverse=True)
-th_score = A[P-1]
-
-def is_pickedup(init_score: int) -> bool:
+def check_pickedup(M: int, V: int, P: int, A: list, accum_score: list, i: int) -> bool:
+    is_pickedup = False
     return True
 
-ans = 0
-was_checked = {}
-for i, a in enumerate(A):
-    if a in was_checked:
-        ans += 1
-        continue
-    if is_pickedup(a):
-        ans += 1
+def main():
+    N, M, V, P = map(int, input().split())
+    A = list(map(int, input().split()))
+    
+    A.sort(reverse=True)
+    th_score = A[P-1]
+    
+    accum_score = [A[0]] * N
+    for i in (1, N):
+        accum_score[i] = accum_score[i-1] + A[i]
+    
+    # Reduce N-V scores each judge
+    ans = 0
+    was_checked = {}
+    for i, a in enumerate(A):
+        # Check a will be picked up
+        if a in was_checked:
+            ans += 1
+            continue
+        if check_pickedup(M, V, P, A, accum_score, i):
+            ans += 1
+            was_checked[a] = True
+        else:
+            break
+    print(ans)
 
-print(ans)
+if __name__ == '__main__':
+    main()
